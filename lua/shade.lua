6433e8cd4332108ebe3f03dd8add92c83421060e
Daniel Tingelöf <daniel.tingelof@ericsson.com>
2022-04-20 16:06:03 +0200

    Fixes

--- lua/shade.lua
+++ lua/shade.lua
@@ -393,9 +393,11 @@ shade.on_win_closed = function(event, winid)
 	if overlay == nil then
 		log(event, "no overlay to close")
 	else
-		api.nvim_win_close(overlay.winid, false)
-		log(event, ("[%d] : overlay %d destroyed"):format(winid, overlay.winid))
-		state.active_overlays[winid] = nil
+		if api.nvim_win_is_valid(winid) then
+			api.nvim_win_close(overlay.winid, false)
+			log(event, ("[%d] : overlay %d destroyed"):format(winid, overlay.winid))
+			state.active_overlays[winid] = nil
+		end
 	end
 end
 
